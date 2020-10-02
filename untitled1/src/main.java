59f986d55474eb00000c607ee413bed784430e47
Jakub Nowak <nowakjakub095@gmail.com>
2020-10-02 12:17:22 +0200

    gitignore

---
+++ untitled1/src/main.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+class main {
+    public static void main(String[] args) {
+    for ( int i = 0; i <10 ; i = i++){
+        i+=1;
+        System.out.println("a");
+    }
+    }
+}
